c1b6d710c8f76b01349e09e4cedc9a8186486770
Apollo Maria <apollomscarvalho@gmail.com>
2026-05-18 18:50:57 -0300

    Add files via upload

---
+++ AEP/script.js
@@ -0,0 +1,168 @@
+// LISTA DE USUÁRIOS -------------------------------------------------------------------------------------
+
+
+const usuario = [
+  {
+    id: 1,
+    nome: "Milton",
+    cpf: "321.321.321-32",
+    dataNascimento: "18/12/2005",
+    username: "milton",
+    telefone: "44 77777-7777",
+    senha: "12345678",
+    email: "milton@gmail.com",
+  },
+
+  {
+    id: 2,
+    nome: "Nicole",
+    cpf: "123.123.123-12",
+    dataNascimento: "01/09/2007",
+    username: "nicole",
+    telefone: "44 88888-8888",
+    senha: "87654321",
+    email: "nicole@email.com",
+  },
+
+  {
+    id: 3,
+    nome: "Apollo",
+    cpf: "666.666.666-66",
+    dataNascimento: "03/07/2004",
+    username: "apollo",
+    telefone: "44 99999-9999",
+    senha: "123456",
+    email: "apollo@email.com",
+  },
+
+];
+
+
+// LISTA DE CARONAS -------------------------------------------------------------------------------------
+
+
+const carona = [
+  {
+    id: 1,
+    qtdPassageiro: 2,
+    origem: "Terminal Intermodal",
+    destino: "Unicesumar",
+    horarioSaida: "07:10",
+    horarioChegada: "07:45",
+    data: "20/07/2026",
+  },
+
+  {
+    id: 2,
+    qtdPassageiro: 3,
+    origem: "Unicesumar",
+    destino: "Terminal Intermodal",
+    horarioSaida: "11:55",
+    horarioChegada: "12:30",
+    data: "20/07/2026",
+  },
+
+  {
+    id: 3,
+    qtdPassageiro: 1,
+    origem: "UEM",
+    destino: "Avenida Center",
+    horarioSaida: "17:20",
+    horarioChegada: "17:55",
+    data: "21/07/2026",
+  },
+
+  {
+    id: 4,
+    qtdPassageiro: 4,
+    origem: "Catedral",
+    destino: "Parque do Japão",
+    horarioSaida: "08:30",
+    horarioChegada: "09:05",
+    data: "21/07/2026",
+  },
+
+  {
+    id: 5,
+    qtdPassageiro: 2,
+    origem: "Praça Farroupilha", 
+    destino: "UEM",
+    horarioSaida: "13:15",
+    horarioChegada: "13:50",
+    data: "22/07/2026",
+  },
+
+  {
+    id: 6,
+    qtdPassageiro: 3,
+    origem: "Willie Davids",
+    destino: "Zona 4",
+    horarioSaida: "18:00",
+    horarioChegada: "18:35",
+    data: "22/07/2026",
+  },
+];
+
+
+
+// LISTA DE CARROS -------------------------------------------------------------------------------------
+
+
+const carro = [
+  {
+    placa: "ABC1D23",
+    marca: "Chevrolet",
+    cor: "prata",
+    modelo: "celta",
+  },
+
+  {
+    placa: "DEF4G56",
+    marca: "Fiat",
+    cor: "preto",
+    modelo: "palio",
+  },
+
+  {
+    placa: "GHI7J89",
+    marca: "Volkswagen",
+    cor: "branco",
+    modelo: "gol",
+  },
+
+  {
+    placa: "JKL0M12",
+    marca: "Ford",
+    cor: "vermelho",
+    modelo: "ka",
+  },
+
+  {
+    placa: "MNO3P45",
+    marca: "Renault",
+    cor: "azul",
+    modelo: "sandero",
+  }
+];
+
+
+// LISTA DE ENDEREÇOS -------------------------------------------------------------------------------------
+
+
+const endereco = [
+  {
+    rua: "Rua dos Bobos",
+    bairro: "Centro",
+    cep: "12345-678",
+    numero: "0",
+    complemento: "Casa",
+  },
+
+  {
+    rua: "Avenida Principal",
+    bairro: "Jardim das Flores",
+    cep: "98765-432",
+    numero: "123",
+    complemento: "Apartamento 456",
+  }
+];
